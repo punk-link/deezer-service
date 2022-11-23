751cd2ed6f84b699e5dfff6fd82364e55b7a41a2
Kirill Taran <kirilltaran@hotmail.com>
2022-11-24 02:29:50 +0400

    Runtime support, part 2

--- go.mod
+++ go.mod
@@ -7,21 +7,22 @@ require github.com/punk-link/logger v1.0.4
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/getsentry/sentry-go v0.15.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.10.0 // indirect
-	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/go-playground/validator/v10 v10.11.1 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/prometheus/client_golang v1.13.0 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
@@ -59,7 +60,7 @@ require (
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/punk-link/consul-client v1.0.2
+	github.com/punk-link/consul-client v1.0.2 // indirect
 	github.com/punk-link/environment-variable-manager v1.0.0 // indirect
 	github.com/punk-link/http-client v1.0.0
 	github.com/punk-link/platform-contracts v1.0.7
--- main.go
+++ main.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"fmt"
 	"main/services"
-	"net/http"
-	"os"
-	"os/signal"
-	"sync"
-	"syscall"
 
-	consulclient "github.com/punk-link/consul-client"
 	httpclient "github.com/punk-link/http-client"
 	"github.com/punk-link/logger"
-	contracts "github.com/punk-link/platform-contracts"
+
+	runtime "github.com/punk-link/streaming-platform-runtime"
 	common "github.com/punk-link/streaming-platform-runtime/common"
-	processing "github.com/punk-link/streaming-platform-runtime/processing"
 	"github.com/punk-link/streaming-platform-runtime/startup"
 )
 
@@ -24,55 +15,12 @@ func main() {
 	logger := logger.New()
 	environmentName := common.GetEnvironmentName()
 	logger.LogInfo("%s is running as '%s'", SERVICE_NAME, environmentName)
-	consul := common.GetConsulClient(logger, SERVICE_NAME, environmentName)
+
+	serviceOptions := runtime.NewServiceOptions(logger, environmentName, SERVICE_NAME)
+	startup.RunServer(serviceOptions)
 
 	deezerService := services.NewDeezerService(logger, httpclient.DefaultConfig(logger))
-	process(logger, consul, deezerService)
-}
-
-func process(logger logger.Logger, consul *consulclient.ConsulClient, service contracts.Platformer) {
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	hostSettingsValues, err := consul.Get("HostSettings")
-	if err != nil {
-		logger.LogFatal(err, "Can't obtain host settings from Consul: '%s'", err.Error())
-	}
-	hostSettings := hostSettingsValues.(map[string]any)
-
-	app := startup.Configure(logger, consul, &startup.StartupOptions{
-		EnvironmentName: "",
-		GinMode:         hostSettings["Mode"].(string),
-		ServiceName:     SERVICE_NAME,
-	})
-	app.Run()
-
-	hostAddress := hostSettings["Address"]
-	hostPort := hostSettings["Port"]
-	server := &http.Server{
-		Addr:    fmt.Sprintf("%s:%s", hostAddress, hostPort),
-		Handler: app,
-	}
-
-	go func() {
-		logger.LogInfo("Starting...")
-		err := server.ListenAndServe()
-		if err != nil && errors.Is(err, http.ErrServerClosed) {
-			logger.LogError(err, "Listen error: %s\n", err.Error())
-		}
-	}()
-
-	natsConnection := common.GetNatsConnection(logger, consul)
-	queueProcessingService := processing.New(logger, natsConnection)
-	go queueProcessingService.Process(ctx, &wg, service)
-
-	wg.Wait()
-	logger.LogInfo("Exiting...")
+	startup.ProcessUrls(serviceOptions, deezerService)
 }
 
 const SERVICE_NAME = "deezer-service"
